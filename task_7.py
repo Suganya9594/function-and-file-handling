f723d4b9aa2bf4232bd617af1e3bfcd8ad792eae
Suganya9594 <suganyaramlit@gmail.com>
2024-07-12 12:58:40 -0400

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,18 +1,20 @@
+
+#importing the modules
 from datetime import datetime
 import time
 
-
+#getting current time
 curr_date_time = datetime.now()
 curr_time = time.strftime("%H-%M-%S")
 # print(curr_time)
 
 
-
+#function to create  new text file
 def create_text_file():
     with open(curr_time + ".txt", "w") as f:
         f.write(curr_time)
 
-
+#function to read the created text file
 def read_text_file():
     with open(curr_time + ".txt", "r") as file:
         contents = file.read()
